1d21da55e747e41c8dd20fcc17f2bceb4e6d7765
fei-hsu <ge54sik@mytum.de>
2020-05-12 19:12:15 +0200

    Create Task3.py

---
+++ Project2/Task3.py
@@ -0,0 +1,74 @@
+import time
+import os
+import os.path
+import json 
+import shutil
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+current_path = os.path.join(os.path.dirname(os.path.realpath(__file__)))
+filename = "rimea_06_corner.scenario"
+new_filename = "rimea_06_corner_task3.scenario"
+base_dir = current_path + "/scenarios/"
+
+#scenario scenario file
+in_file = open(base_dir + filename, "r")
+data = json.load(in_file)
+in_file.close()
+
+#rename
+tmp = data["name"]
+data["name"] = "rimea_06_corner_task3.scenario"
+#add pedestrian
+
+tmp = data["scenario"]["topography"]["dynamicElements"]
+ped =[{
+        "source" : None,
+        "targetIds" : [ 2 ],
+        "position" : {
+          "x" : 11.6,
+          "y" : 1.5
+        },
+        "velocity" : {
+          "x" : 0.0,
+          "y" : 0.0
+        },
+        "nextTargetListIndex" : 0,
+        "freeFlowSpeed" : 1.752612703047805,
+        "attributes" : {
+          "id" : -1,
+          "radius" : 0.2,
+          "densityDependentSpeed" : False,
+          "speedDistributionMean" : 1.34,
+          "speedDistributionStandardDeviation" : 0.26,
+          "minimumSpeed" : 0.5,
+          "maximumSpeed" : 2.2,
+          "acceleration" : 2.0,
+          "footStepsToStore" : 4,
+          "searchRadius" : 1.0,
+          "angleCalculationType" : "USE_CENTER",
+          "targetOrientationAngleThreshold" : 45.0
+        },
+        "idAsTarget" : -1,
+        "isChild" : False,
+        "isLikelyInjured" : False,
+        "mostImportantEvent" : None,
+        "salientBehavior" : "TARGET_ORIENTED",
+        "groupIds" : [ ],
+        "trajectory" : {
+          "footSteps" : [ ]
+        },
+        "groupSizes" : [ ],
+        "modelPedestrianMap" : None,
+        "type" : "PEDESTRIAN"
+      }]
+
+data["scenario"]["topography"]["dynamicElements"] = ped
+
+#save the file with new name
+out_file = open(base_dir + new_filename, "w+")
+out_file.write(json.dumps(data))
+out_file.close()
